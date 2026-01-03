caedf4a29e2acb5ae296effb46afecd36d41b667
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2026-01-03 19:41:17 +0530

    login forms

---
+++ vite-project/src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext,useState } from "react";
+export const AuthContext=createContext();
+export const AuthProvider=
+({ children })=>{
+    const [isAuth,setIsAuth]=useState(false);
+    const [role,setRole]=useState(null);
+
+    const login=(email,password)=>{
+        if(email=== "admin@gmail.com" && password ==="admin1234")
+            {
+            setIsAuth(true);
+            setRole("admin");
+            return "admin";
+        }
+        if(email=== "customer@gmail.com" && password ==="customer1234")
+            {
+            setIsAuth(true);
+            setRole("customer");
+            return "customer";
+        }
+        return null;
+        
+    };
+    const logout=()=>{
+        setIsAuth(false);
+        setRole(null);
+    };
+    return (
+        <AuthContext.Provider
+         value={{ isAuth,role,login,logout}}>
+        {childern}
+       <AuthContext.Provider/>
+
+    );
+
+};
