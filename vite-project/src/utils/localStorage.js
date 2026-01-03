87b965f14a16f5f968c687c2cc76075d879b2c2f
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2026-01-03 20:06:06 +0530

    Done with localstorage

---
+++ vite-project/src/utils/localStorage.js
@@ -0,0 +1,10 @@
+const KEY ="evalData"
+export const getRestaurants =() =>{
+    return
+    JSON.parse(localStorage.getItem(KEY)) || [];
+
+};
+
+export const saveRestaurants = (data) =>{
+    localStorage.setItem(KEY,JSON.stringify(data));
+};
