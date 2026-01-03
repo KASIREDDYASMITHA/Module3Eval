24d5c900a6ca666622e4177085010b2ac0fd03ec
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2026-01-03 20:52:26 +0530

    files

--- vite-project/src/pages/CustomerDashboard.jsx
+++ vite-project/src/pages/CustomerDashboard.jsx
@@ -3,3 +3,23 @@ import { getRestaurants } from "../utils/localStorage";
 import RestaurantCard from "../components/RestaurantCard";
 import Navbar from "../components/Navbar";
 
+const CustomerDashboard =() => {
+    const [restaurants, setRestaurants]=useState([]);
+
+    useEffect(() =>{
+        setRestaurants(getRestaurants());
+    },[]);
+    return (
+        <div>
+            <h2>Customer Dshboard</h2>
+            {restaurants.map((r) => (
+                <div key ={r.restaurantID}>
+                    <h4>{r.restaurantNmae}</h4>
+                    <p>{r.address}</p>
+                    </div>
+            ))}
+       
+    );
+};
+
+export default CustomerDashboard;
